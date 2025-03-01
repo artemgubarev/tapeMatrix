c4fba2efa6e1d6ebc0ff3086e9d884fc5b6f18ac
artemgubarev <artem.grvz@gmail.com>
2025-03-01 19:14:22 +0300

    np comp

--- tapeMatrixDemo/comparator.py
+++ tapeMatrixDemo/comparator.py
@@ -1,21 +1,23 @@
 from mpi4py import MPI
+import numpy as np
 
 def compare_numbers(file1, file2, tolerance=1e-5):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+
     if rank == 0:
-        with open(file1, 'r') as f1:
-            numbers1 = list(map(float, f1.read().split()))
-        with open(file2, 'r') as f2:
-            numbers2 = list(map(float, f2.read().split()))
-        if len(numbers1) != len(numbers2):
-            print("test failed")
+        numbers1 = np.loadtxt(file1)
+        numbers2 = np.loadtxt(file2)
+        
+        if numbers1.shape != numbers2.shape:
+            print("test failed: количество чисел в файлах различается")
             return
-        for i, (num1, num2) in enumerate(zip(numbers1, numbers2)):
-            if abs(num1 - num2) > tolerance:
-                print(f"test failed {i}: {num1} != {num2}")
-                return
-        print("test correct")
+        
+        if np.allclose(numbers1, numbers2, atol=tolerance):
+            print("test correct")
+        else:
+            print("test failed: числа в файлах различаются")
     else:
         pass
+    
     comm.Barrier()
